03b2d20bef5eb27150157e2ecdc10884c25c6033
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2026-02-19 22:02:08 -0300

    Aula3

---
+++ Aula3/src/variaveisTipos.ts
@@ -0,0 +1,10 @@
+let nome:string = 'João'
+let idade:number = 25
+let ativo:boolean = true
+
+let dado:any = 10
+let vazio:null = null
+let indefinido: undefined = undefined
+
+let situacao:"ativo" | "inativo" = "ativo"
+let idadeOuNulo: number | null = null
