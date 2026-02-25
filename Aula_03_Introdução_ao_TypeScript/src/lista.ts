a509e72d8fd0a1b3fac34cb6e1756204d5a16dfb
BrunoMendesMorais[ <mendesdemoraisbruno@gmail.com>
2026-02-25 19:49:14 -0300

    Operadores Lista Variaveis

---
+++ Aula_03_Introdução_ao_TypeScript/src/lista.ts
@@ -0,0 +1,12 @@
+let nomes: string[] = ['ana', 'joão', 'alves']
+
+console.log(nomes);
+
+let numeros: number[] = [1,2,3,4,5];
+console.log(numeros[2]);
+
+console.log(nomes.indexOf("joão"));
+
+nomes.splice(1,1)
+
+console.log(nomes);
--- Aula_03_Introdução_ao_TypeScript/src/variaveisTipos.ts
+++ Aula_03_Introdução_ao_TypeScript/src/variaveisTipos.ts
@@ -8,3 +8,4 @@ let indefinido: undefined = undefined
 
 let situacao:"ativo" | "inativo" = "ativo"
 let idadeOuNulo: number | null = null
+
