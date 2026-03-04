3f5a4328a1eaedf3e060ba465380f5117c3fbf55
BrunoMendesMorais <mendesdemoraisbruno@gmail.com>
2026-03-04 20:38:13 -0300

    exercicios proposto

---
+++ Aula_05-Funções_em_Typescript/src/exercicio.ts
@@ -0,0 +1,10 @@
+function maiuscula(palavra:string):string{
+    return  palavra.toUpperCase()
+}
+let maiusculaLambda = (palavra:string)=>(palavra.toUpperCase)
+
+let elevar =(numero:number,expoente:number)=>(numero**expoente)
+
+function dobrarNumeros (numeros:string[]){
+    
+}
